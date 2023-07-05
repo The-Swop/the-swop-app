730c3252f2dd4a832e355a327837702e60731cf0
Sebastian-EP <pedrosaenterprises@gmail.com>
2023-07-05 19:24:31 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { SafeAreaView } from "react-native";
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class MyWeb extends Component {
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <WebView 
+          source={{ uri: 'https://kvyr2-jyaaa-aaaam-qbaca-cai.ic0.app/' }} 
+        />
+      </SafeAreaView>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyWeb;
