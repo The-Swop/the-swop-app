8e053c9b96c303621ab87fbf0390633f8fe6f316
sebastian-ep <pedrosaenterprises@gmail.com>
2023-08-09 20:15:45 -0400

    fixed status bar color mismatch

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { SafeAreaView } from "react-native";
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 class MyWeb extends Component {
   render() {
     return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <WebView 
-          source={{ uri: 'https://aixb6-aqaaa-aaaap-qbjmq-cai.raw.icp0.io/' }} 
-        />
-      </SafeAreaView>
+      <View style={{ flex: 1, backgroundColor: '#000' }}>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1 }}>
+            <WebView source={{ uri: 'https://aixb6-aqaaa-aaaap-qbjmq-cai.raw.icp0.io/' }} />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </View>
     );
   }
 }
